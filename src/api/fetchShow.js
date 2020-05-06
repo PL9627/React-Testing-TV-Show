e438e79d019e6cd3bba670bfe1c19c728a825658
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-06 16:32:21 -0700

    finished with fetchShow.js

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,5 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchShow = () => {
-
+  axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then((res) => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    })
+    .catch(err => console.log("fetchShow axios err", err));
 };
